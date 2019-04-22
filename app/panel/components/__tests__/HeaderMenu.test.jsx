310d5cbd1165f188bcd537e5374dfe443456fdad
Anri Zeneli <azeneli46@gmail.com>
2019-04-22 16:32:33 -0400

    fix path and remove skip on test suite

--- app/panel/components/__tests__/HeaderMenu.test.jsx
+++ app/panel/components/__tests__/HeaderMenu.test.jsx
@@ -4,11 +4,12 @@ import ClassNames from 'classnames';
 
 import ClickOutside from '../BuildingBlocks/ClickOutside';
 import HeaderMenu from '../HeaderMenu';
-import { log } from '../../../src/utils/common';
+import { sendMessage, sendMessageInPromise } from '../../utils/msg';
+import { log } from '../../../../src/utils/common';
 import globals from '../../../../src/classes/Globals';
-import { sendMessage, sendMessageInPromise } from '../utils/msg';
 
-describe.skip('app/panel/components/HeaderMenu', () => {
+
+describe('app/panel/components/HeaderMenu', () => {
 	describe('Snapshot tests with react-test-renderer', () => {
 		test('HeaderMenu is rendering correctly', () => {
 			const component = renderer.create(
