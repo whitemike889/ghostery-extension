b31e1a5ea3d72e22a9bc08928dee5aa3db848671
Anri Zeneli <azeneli46@gmail.com>
2019-04-22 11:40:39 -0400

    ongoing testing for panel actions unit tests

--- app/panel/actions/__tests__/PanelActions.js
+++ app/panel/actions/__tests__/PanelActions.js
@@ -15,6 +15,10 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import * as panelActions from '../PanelActions';
 import { TOGGLE_CLIQZ_FEATURE } from '../../constants/constants';
+import { UPDATE_PANEL_DATA } from '../../constants/constants';
+import { SHOW_NOTIFICATION } from '../../constants/constants';
+import { CLOSE_NOTIFICATION } from '../../constants/constants';
+import { TOGGLE_EXPERT } from '../../constants/constants';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -34,4 +38,58 @@ describe('app/panel/actions/PanelActions.js', () => {
 		const actions = store.getActions();
 		expect(actions).toEqual([expectedPayload]);
 	});
+
+	test('updatePanelData action should resolve', () => {
+		const initialState = {};
+		const store = mockStore(initialState);	
+		const data = {
+			test: 'test-data'
+		}
+
+		const expectedPayload = { data, type: UPDATE_PANEL_DATA };
+		store.dispatch(panelActions.updatePanelData(data));
+
+		const actions = store.getActions();
+		expect(actions).toEqual([expectedPayload]);
+	});
+
+	test('showNotification action should resolve', () => {
+		const initialState = {};
+		const store = mockStore(initialState);
+		const data = {
+			test: 'test-data'
+		}
+
+		const expectedPayload = {data, type: SHOW_NOTIFICATION};
+		store.dispatch(panelActions.showNotification(data));
+
+		const action = store.getActions();
+		expect(action).toEqual([expectedPayload]);
+	});
+
+	test('closeNotification action should resolve', () => {
+		const initialState = {};
+		const store = mockStore(initialState);
+		const data = {
+			test: 'test-data'
+		}
+
+		const expectedPayload = {data, type: CLOSE_NOTIFICATION};
+		store.dispatch(panelActions.closeNotification(data));
+
+		const action = store.getActions();
+		expect(action).toEqual([expectedPayload]);
+	});
+
+	test.only('toggleExpect action should resolve', () => {
+		const initialState = {};
+		const store = mockStore(initialState);
+
+		const expectedPayload = {type: TOGGLE_EXPERT};
+		store.dispatch(panelActions.toggleExpert());
+
+		const action = store.getActions();
+		expect(action).toEqual([expectedPayload]);
+	});
+
 });
