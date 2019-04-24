de715e9418360163b0a45ff11355179b550d7d3e
Anri Zeneli <azeneli46@gmail.com>
2019-04-24 16:27:01 -0400

    add unit test for getTheme success action

--- app/panel/actions/__tests__/PanelActions.js
+++ app/panel/actions/__tests__/PanelActions.js
@@ -13,17 +13,23 @@
 
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import * as msg from '../../utils/msg';
 import * as panelActions from '../PanelActions';
-import { TOGGLE_CLIQZ_FEATURE } from '../../constants/constants';
-import { UPDATE_PANEL_DATA } from '../../constants/constants';
-import { SHOW_NOTIFICATION } from '../../constants/constants';
-import { CLOSE_NOTIFICATION } from '../../constants/constants';
-import { TOGGLE_EXPERT } from '../../constants/constants';
+import {
+	TOGGLE_CLIQZ_FEATURE,
+	UPDATE_PANEL_DATA,	
+	SHOW_NOTIFICATION,
+	CLOSE_NOTIFICATION,
+	TOGGLE_EXPERT,
+	SET_THEME,
+	CLEAR_THEME	
+} from '../../constants/constants';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
 describe('app/panel/actions/PanelActions.js', () => {
+
 	test('toggleCliqzFeature action should resolve correctly', () => {
 		const initialState = {};
 		const store = mockStore(initialState);
@@ -41,7 +47,7 @@ describe('app/panel/actions/PanelActions.js', () => {
 
 	test('updatePanelData action should resolve', () => {
 		const initialState = {};
-		const store = mockStore(initialState);	
+		const store = mockStore(initialState);
 		const data = {
 			test: 'test-data'
 		}
@@ -81,7 +87,7 @@ describe('app/panel/actions/PanelActions.js', () => {
 		expect(action).toEqual([expectedPayload]);
 	});
 
-	test.only('toggleExpect action should resolve', () => {
+	test('toggleExpect action should resolve', () => {
 		const initialState = {};
 		const store = mockStore(initialState);
 
@@ -91,5 +97,53 @@ describe('app/panel/actions/PanelActions.js', () => {
 		const action = store.getActions();
 		expect(action).toEqual([expectedPayload]);
 	});
+
+	test('getTheme action should resolve', () => {
+		const initialState = {};
+		const store = mockStore(initialState);
+
+		const data = { test: 'test-theme' };
+		const expectedPayload = { data, type: SET_THEME };
+
+		msg.sendMessageInPromise = jest.fn(name => new Promise((resolve) => {
+			switch (name) {
+				case 'getTheme':
+					resolve(data);
+					break;
+				case 'account.getTheme': 
+					resolve(data);
+				default:
+					resolve();
+			}
+		}));
+
+		return store.dispatch(panelActions.getTheme(data)).then(() => {
+			const actions = store.getActions();
+			expect(actions).toEqual([expectedPayload]);
+		});
+
+	});
+
+	// should resolve data
+
+	// test.only('getTheme action should resolve', () => {
+	// 	const initialState = {};
+	// 	const store = mockStore(initialState);
+
+	// 	const data = { test: 'test-theme' };
+	// 	const expectedPayload = { data, type: SET_THEME };
+
+	// 	msg.sendMessageInPromise = jest.fn(name => new Promise((resolve) => {
+	// 		resolve();
+	// 		}
+	// 	}));
+
+	// 	// maybe it is failing bc you would need to be a subscribed user in order to change the theme
+	// 	return store.dispatch(panelActions.getTheme(data)).then(() => {
+	// 		const actions = store.getActions();
+	// 		expect(actions).toEqual([expectedPayload]);
+	// 	});
+
+	// });
 
 });
