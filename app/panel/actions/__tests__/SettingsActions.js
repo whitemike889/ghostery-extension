5f37e009ce49d9574067107246402f13b213612c
Anri Zeneli <azeneli46@gmail.com>
2019-05-06 12:58:15 -0400

    unit test for togglecheckbox

--- app/panel/actions/__tests__/SettingsActions.js
+++ app/panel/actions/__tests__/SettingsActions.js
@@ -33,7 +33,9 @@ import {
 	GET_SETTINGS_DATA,
 	DO_NOTHING
 } from '../../constants/constants';
-import { sendMessageInPromise } from '../../utils/msg';
+// import { sendMessageInPromise } from '../../utils/msg';
+// import bannerStatus from '../../reducers/settings';
+import * as msg from '../../utils/msg';
 import { hashCode } from '../../../../src/utils/common';
 import globals from '../../../../src/classes/Globals';
 
@@ -56,6 +58,112 @@ describe('app/panel/actions/SettingsActions.js', () => {
 		const actions = store.getActions();
 		expect(actions).toEqual([expectedPayload]);
 	});
+
+	test.only('toggleCheckbox should resolve', () => {
+		let type;
+		const initialState = {};
+		const store = mockStore(initialState);
+		let data = { test: 'test-data' };
+		const expectedPayload = {
+			data, 
+			type: TOGGLE_CHECKBOX
+		};
+	
+		store.dispatch(settingsActions.toggleCheckbox(data));
+
+		const actions = store.getActions();	
+		expect(actions).toEqual([expectedPayload]);
+	});
+
+	test('toggleCheckbox should resolve with type trackers_banner_status', () => {
+		let type;
+		const initialState = {};
+		const store = mockStore(initialState);
+		let data = {
+			test: 'test-data',
+			event: 'trackers_banner_status'
+		}
+
+		if(data.event === 'trackers_banner_status'){
+			type = UPDATE_NOTIFICATION_STATUS;
+		}
+
+		const expectedPayload = {
+			data, 
+			type: UPDATE_NOTIFICATION_STATUS
+		}
+
+		store.dispatch(settingsActions.toggleCheckbox(data));
+
+		const actions = store.getActions();
+		expect(actions).toEqual([expectedPayload]);
+	});
+
+	test('toggleCheckbox should resolve with type reload_banner_status', () => {
+		let type;
+		const initialState = {};
+		const store = mockStore(initialState);
+		let data = {
+			test: 'test-data',
+			event: 'reload_banner_status'
+		}
+
+		if(data.event === 'reload_banner_status'){
+			type = UPDATE_NOTIFICATION_STATUS;
+		}
+
+		const expectedPayload = {
+			data, 
+			type: UPDATE_NOTIFICATION_STATUS
+		}
+
+		store.dispatch(settingsActions.toggleCheckbox(data));
+
+		const actions = store.getActions();
+		expect(actions).toEqual([expectedPayload]);
+	});
+
+
+
+	test.skip('updateDatabase should resolve', () => {
+		const initialState = {};
+		const store = mockStore(initialState);
+		let resultText;
+		const expectedPayload = {
+			resultText,
+			type: UPDATE_DATABASE
+		}	
+
+		// msg.sendMessageInPromise = jest.fn(resultText => new Promise((result) => {
+		// 	switch (resultText) {
+		// 		case 'settings_update_success':
+		// 			if(result && result.success === true) {
+		// 				if(result.updated === true){
+		// 					resultText = 'settings_update_success';
+		// 				}
+		// 			}	
+		// 			//resolves(result);			
+		// 			//break;
+		// 		case 'settings_update_up_to_date': 
+		// 			if(result && result.success !== true){
+		// 				resultText = 'settings_update_up_to_date';
+		// 			}	
+		// 			//resolves(result);
+		// 		default:
+		// 			resultText = 'settings_update_failed';
+		// 			//resolves();
+		// 	}
+		// }));
+
+		// return store.dispatch(settingsActions.updateDatabase());
+		// 	const actions = store.getActions();
+		// 	expect(actions).toEqual([expectedPayload]);
+		// });
+		expect(true).toBe(true);
+	});
+
+
+
 
 	test('updateBlockAllTrackers should resolve', () => {
 		const initialState = {};
@@ -182,13 +290,3 @@ describe('app/panel/actions/SettingsActions.js', () => {
 
 
 });
-
-
-
-
-
-
-
-
-
-
