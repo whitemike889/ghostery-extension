8a49cf5a7f273fcdad0024a6ddeb9faa58de53e5
Anri Zeneli <azeneli46@gmail.com>
2019-06-28 11:07:47 -0400

    fix liniting error, remove shallow and rename test title

--- app/panel/components/__tests__/Help.jsx
+++ app/panel/components/__tests__/Help.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
 import Help from '../Help';
 
 describe('app/panel/components/Subscription Help Component', () => {
 	describe('Snapshot Test with react-test-render', () => {
-			test('welp the help view', () => {
-				const component = renderer.create(
-					<Help openHubTab={() => {}} openSupportTab={() => {}}/>
-				).toJSON(); 
+		test('help view should render', () => {
+			const component = renderer.create(
+				<Help openHubTab={() => {}} openSupportTab={() => {}} />
+			).toJSON();
 			expect(component).toMatchSnapshot();
 		});
 	});
