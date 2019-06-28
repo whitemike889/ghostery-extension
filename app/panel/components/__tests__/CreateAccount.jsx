c35bdc90af852c92a1708dac5df371169fb6ceaa
Anri Zeneli <azeneli46@gmail.com>
2019-06-28 11:42:22 -0400

    fix linting errors, remove imports

--- app/panel/components/__tests__/CreateAccount.jsx
+++ app/panel/components/__tests__/CreateAccount.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { MemoryRouter } from 'react-router';
-import { shallow, mount } from 'enzyme';
-import ClassNames from 'classnames';
-import RSVP from 'rsvp';
+import { shallow } from 'enzyme';
 
-
-import { validateEmail, validateConfirmEmail, validatePassword } from '../../utils/utils';
 import CreateAccount from '../CreateAccount';
 
 describe('app/panel/components/CreateAccount Component', () => {
 	describe('Snapshot test with react-test-renderer', () => {
 		test('Testing account creation page is rendering', () => {
-			const initialState= {
+			const initialState = {
 				email: '',
 				emailError: false,
 				confirmEmail: '',
@@ -24,18 +20,16 @@ describe('app/panel/components/CreateAccount Component', () => {
 				loading: false,
 				passwordInvalidError: false,
 				passwordLengthError: false,
-			}
+			};
 
 			const component = renderer.create(
-				shallow(<MemoryRouter>
-					<CreateAccount {...initialState} />
-				</MemoryRouter>)	
-			).toJSON(); 
+				shallow(<MemoryRouter><CreateAccount {...initialState} /></MemoryRouter>)
+			).toJSON();
 			expect(component).toMatchSnapshot();
 		});
 
 		test('Testing the account creation page is rendering with the errors showing', () => {
-			const initialState= {
+			const initialState = {
 				email: '',
 				emailError: true,
 				confirmEmail: '',
@@ -47,20 +41,18 @@ describe('app/panel/components/CreateAccount Component', () => {
 				loading: true,
 				passwordInvalidError: true,
 				passwordLengthError: true,
-			}
+			};
 
 			const component = renderer.create(
-				shallow(<MemoryRouter>
-					<CreateAccount {...initialState} />
-				</MemoryRouter>)	
-			).toJSON(); 
+				shallow(<MemoryRouter><CreateAccount {...initialState} /></MemoryRouter>)
+			).toJSON();
 			expect(component).toMatchSnapshot();
 		});
-	});	
+	});
 
 	describe('Testing the handling of data input and submitting', () => {
 		test('testing the input of email and password without first/last name', () => {
-			const initialState= {
+			const initialState = {
 				email: '',
 				emailError: false,
 				confirmEmail: '',
@@ -72,29 +64,29 @@ describe('app/panel/components/CreateAccount Component', () => {
 				loading: false,
 				passwordInvalidError: false,
 				passwordLengthError: false,
-			}	
+			};
 
 			const wrapper = shallow(<CreateAccount {...initialState} />);
 
 			const email = 'test@example.com';
 			expect(wrapper.state('email')).toEqual('');
-			wrapper.find('#create-input-email').simulate('change', {target: {name: 'email', value: email }});
+			wrapper.find('#create-input-email').simulate('change', { target: { name: 'email', value: email } });
 			expect(wrapper.state('email')).toEqual(email);
 
 			expect(wrapper.state('confirmEmail')).toEqual('');
-			wrapper.find('#create-input-email-confirm').simulate('change', {target: {name: 'confirmEmail', value: email }});
+			wrapper.find('#create-input-email-confirm').simulate('change', { target: { name: 'confirmEmail', value: email } });
 			expect(wrapper.state('confirmEmail')).toEqual(email);
 
 			expect(wrapper.state('promotionsChecked')).toBe(true);
 
 			const password = 'admin';
 			expect(wrapper.state('password')).toEqual('');
-			wrapper.find('input[type="password"]').simulate('change', {target: {name: 'password', value: password }});
+			wrapper.find('input[type="password"]').simulate('change', { target: { name: 'password', value: password } });
 			expect(wrapper.state('password')).toEqual(password);
 		});
 
 		test('testing the input of email and password with first/last name', () => {
-			const initialState= {
+			const initialState = {
 				email: '',
 				emailError: false,
 				confirmEmail: '',
@@ -106,39 +98,39 @@ describe('app/panel/components/CreateAccount Component', () => {
 				loading: false,
 				passwordInvalidError: false,
 				passwordLengthError: false,
-			}
+			};
 
 			const wrapper = shallow(<CreateAccount {...initialState} />);
 
 			const email = 'test@example.com';
 			expect(wrapper.state('email')).toEqual('');
-			wrapper.find('#create-input-email').simulate('change', {target: {name: 'email', value: email }});
+			wrapper.find('#create-input-email').simulate('change', { target: { name: 'email', value: email } });
 			expect(wrapper.state('email')).toEqual(email);
 
 			expect(wrapper.state('confirmEmail')).toEqual('');
-			wrapper.find('#create-input-email-confirm').simulate('change', {target: {name: 'confirmEmail', value: email }});
+			wrapper.find('#create-input-email-confirm').simulate('change', { target: { name: 'confirmEmail', value: email } });
 			expect(wrapper.state('confirmEmail')).toEqual(email);
 
 			const firstName = 'John';
 			expect(wrapper.state('firstName')).toEqual('');
-			wrapper.find('#create-input-first-name').simulate('change', {target: {name: 'firstName', value: firstName }});
+			wrapper.find('#create-input-first-name').simulate('change', { target: { name: 'firstName', value: firstName } });
 			expect(wrapper.state('firstName')).toEqual(firstName);
 
 			const lastName = 'Doe';
 			expect(wrapper.state('lastName')).toEqual('');
-			wrapper.find('#create-input-last-name').simulate('change', {target: {name: 'lastName', value: lastName }});
+			wrapper.find('#create-input-last-name').simulate('change', { target: { name: 'lastName', value: lastName } });
 			expect(wrapper.state('lastName')).toEqual(lastName);
 
 			expect(wrapper.state('promotionsChecked')).toBe(true);
 
 			const password = 'admin';
 			expect(wrapper.state('password')).toEqual('');
-			wrapper.find('input[type="password"]').simulate('change', {target: {name: 'password', value: password }});
+			wrapper.find('input[type="password"]').simulate('change', { target: { name: 'password', value: password } });
 			expect(wrapper.state('password')).toEqual(password);
 		});
 
 		test('testing the creation of an account with correct data', () => {
-			const initialState= {
+			const initialState = {
 				email: '',
 				emailError: false,
 				confirmEmail: '',
@@ -150,18 +142,18 @@ describe('app/panel/components/CreateAccount Component', () => {
 				loading: false,
 				passwordInvalidError: false,
 				passwordLengthError: false
-			}				
+			};
 
 			const wrapper = shallow(<CreateAccount {...initialState} />);
-			const myMock = jest.fn();	
+			const myMock = jest.fn();
 			wrapper.instance().handleSubmit = myMock;
 			wrapper.instance().forceUpdate();
 
-			wrapper.find('#create-input-email').simulate('change', {target: {name: 'email', value: 'test@example.com'}});
-			wrapper.find('#create-input-email-confirm').simulate('change', {target: {name: 'confirmEmail', value: 'test@example.com'}});
-			wrapper.find('#create-input-first-name').simulate('change', {target: {name: 'firstName', value: 'John'}});
-			wrapper.find('#create-input-last-name').simulate('change', {target: {name: 'lastName', value: 'Doe'}});	
-			wrapper.find('input[type="password"]').simulate('change', {target: {name: 'password', value: 'admin'}});			
+			wrapper.find('#create-input-email').simulate('change', { target: { name: 'email', value: 'test@example.com' } });
+			wrapper.find('#create-input-email-confirm').simulate('change', { target: { name: 'confirmEmail', value: 'test@example.com' } });
+			wrapper.find('#create-input-first-name').simulate('change', { target: { name: 'firstName', value: 'John' } });
+			wrapper.find('#create-input-last-name').simulate('change', { target: { name: 'lastName', value: 'Doe' } });
+			wrapper.find('input[type="password"]').simulate('change', { target: { name: 'password', value: 'admin' } });
 
 			expect(wrapper.state('promotionsChecked')).toBe(true);
 			expect(myMock.mock.calls.length).toBe(0);
@@ -176,7 +168,7 @@ describe('app/panel/components/CreateAccount Component', () => {
 		});
 
 		test('testing the creation of account with checkbox unchecked', () => {
-			const initialState= {
+			const initialState = {
 				email: '',
 				emailError: false,
 				confirmEmail: '',
@@ -188,21 +180,21 @@ describe('app/panel/components/CreateAccount Component', () => {
 				loading: false,
 				passwordInvalidError: false,
 				passwordLengthError: false
-			}				
+			};
 
 			const wrapper = shallow(<CreateAccount {...initialState} />);
-			const myMock = jest.fn();	
+			const myMock = jest.fn();
 			wrapper.instance().handleSubmit = myMock;
 			wrapper.instance().forceUpdate();
 
-			wrapper.find('#create-input-email').simulate('change', {target: {name: 'email', value: 'test@example.com'}});
-			wrapper.find('#create-input-email-confirm').simulate('change', {target: {name: 'confirmEmail', value: 'test@example.com'}});
-			wrapper.find('#create-input-first-name').simulate('change', {target: {name: 'firstName', value: 'John'}});
-			wrapper.find('#create-input-last-name').simulate('change', {target: {name: 'lastName', value: 'Doe'}});	
-			wrapper.find('input[type="password"]').simulate('change', {target: {name: 'password', value: 'admin'}});			
+			wrapper.find('#create-input-email').simulate('change', { target: { name: 'email', value: 'test@example.com' } });
+			wrapper.find('#create-input-email-confirm').simulate('change', { target: { name: 'confirmEmail', value: 'test@example.com' } });
+			wrapper.find('#create-input-first-name').simulate('change', { target: { name: 'firstName', value: 'John' } });
+			wrapper.find('#create-input-last-name').simulate('change', { target: { name: 'lastName', value: 'Doe' } });
+			wrapper.find('input[type="password"]').simulate('change', { target: { name: 'password', value: 'admin' } });
 
-			wrapper.find('#promotionsChecked').simulate('change',{ target: {name: 'promotionsChecked', checked: false} } );
-			expect(myMock.mock.calls.length).toBe(0);	
+			wrapper.find('#promotionsChecked').simulate('change', { target: { name: 'promotionsChecked', checked: false } });
+			expect(myMock.mock.calls.length).toBe(0);
 
 			wrapper.find('form').simulate('submit');
 			expect(myMock.mock.calls.length).toBe(1);
@@ -210,7 +202,7 @@ describe('app/panel/components/CreateAccount Component', () => {
 		});
 
 		test('testing the creation of account with invalid email', () => {
-			const initialState= {
+			const initialState = {
 				email: '',
 				emailError: false,
 				confirmEmail: '',
@@ -222,35 +214,37 @@ describe('app/panel/components/CreateAccount Component', () => {
 				loading: false,
 				passwordInvalidError: false,
 				passwordLengthError: false,
-			}
+			};
 
-			const wrapper = shallow( <CreateAccount {...initialState} />);
+			const wrapper = shallow(<CreateAccount {...initialState} />);
 			wrapper.find('#create-input-email').simulate('change', {
-					target: {
-						name: 'email', 
-						value: 'test1'
-					}});
-			wrapper.find('#create-input-email-confirm').simulate('change', {target: {name: 'confirmEmail', value: 'test1'}});
+				target: {
+					name: 'email',
+					value: 'test1'
+				}
+			});
+			wrapper.find('#create-input-email-confirm').simulate('change', { target: { name: 'confirmEmail', value: 'test1' } });
 			wrapper.find('input[type="password"]').simulate('change', {
-					target: {
-						name: 'password', 
-						value: 'hi'
-					}});
+				target: {
+					name: 'password',
+					value: 'hi'
+				}
+			});
 			expect(wrapper.state('promotionsChecked')).toBe(true);
 
 			const preventDefault = jest.fn();
 			expect(preventDefault.mock.calls.length).toBe(0);
 
 
-			wrapper.find('form').simulate('submit',  { preventDefault });
+			wrapper.find('form').simulate('submit', { preventDefault });
 			expect(preventDefault.mock.calls.length).toBe(1);
-		
+
 			expect(wrapper.state('emailError')).toBe(true);
-		});	
+		});
 
 
 		test('testing the creation of account with invalid password', () => {
-			const initialState= {
+			const initialState = {
 				email: '',
 				emailError: false,
 				confirmEmail: '',
@@ -262,30 +256,32 @@ describe('app/panel/components/CreateAccount Component', () => {
 				loading: false,
 				passwordInvalidError: false,
 				passwordLengthError: false,
-			}
+			};
 
-			const wrapper = shallow( <CreateAccount {...initialState} />);
+			const wrapper = shallow(<CreateAccount {...initialState} />);
 			wrapper.find('#create-input-email').simulate('change', {
-					target: {
-						name: 'email', 
-						value: 'test@example.com'
-					}});
-			wrapper.find('#create-input-email-confirm').simulate('change', {target: {name: 'confirmEmail', value: 'test@example.com'}});
+				target: {
+					name: 'email',
+					value: 'test@example.com'
+				}
+			});
+			wrapper.find('#create-input-email-confirm').simulate('change', { target: { name: 'confirmEmail', value: 'test@example.com' } });
 			wrapper.find('input[type="password"]').simulate('change', {
-					target: {
-						name: 'password', 
-						value: 'hi'
-					}});
+				target: {
+					name: 'password',
+					value: 'hi'
+				}
+			});
 			expect(wrapper.state('promotionsChecked')).toBe(true);
 
 			const preventDefault = jest.fn();
 			expect(preventDefault.mock.calls.length).toBe(0);
 
 
-			wrapper.find('form').simulate('submit',  { preventDefault });
+			wrapper.find('form').simulate('submit', { preventDefault });
 			expect(preventDefault.mock.calls.length).toBe(1);
-		
-			expect(wrapper.state('passwordLengthError')).toBe(true);	
+
+			expect(wrapper.state('passwordLengthError')).toBe(true);
 		});
-	});	
+	});
 });
--- app/panel/components/__tests__/HeaderMenu.jsx
+++ app/panel/components/__tests__/HeaderMenu.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import ClassNames from 'classnames';
 
-import ClickOutside from '../BuildingBlocks/ClickOutside';
 import HeaderMenu from '../HeaderMenu';
-import { sendMessage, sendMessageInPromise } from '../../utils/msg';
-import { log } from '../../../../src/utils/common';
-import globals from '../../../../src/classes/Globals';
-
 
 describe('app/panel/components/HeaderMenu', () => {
 	describe('Snapshot tests with react-test-renderer', () => {
 		test('HeaderMenu is rendering correctly', () => {
 			const component = renderer.create(
-				<HeaderMenu  toggleDropDown = {() => {}} clickBrokenPage = {() => {}}
-					clickSubmitTracker = {() => {}} clickHelp = {() => {}}
-					clickAbout = {() => {}} clickSubscriber = {() => {}}	
+				<HeaderMenu
+					toggleDropDown={() => {}}
+					clickBrokenPage={() => {}}
+					clickSubmitTracker={() => {}}
+					clickHelp={() => {}}
+					clickAbout={() => {}}
+					clickSubscriber={() => {}}
 				/>
 			).toJSON();
 			expect(component).toMatchSnapshot();
