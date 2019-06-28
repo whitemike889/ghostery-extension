1723f518dce4eabff6a847b99bfcc982f1d6925a
Anri Zeneli <azeneli46@gmail.com>
2019-06-28 11:13:11 -0400

    remove shallow and another mock that was not being used

--- app/panel/components/__tests__/About.jsx
+++ app/panel/components/__tests__/About.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
 import About from '../About';
 
 
 // Importing Mock Data
-jest.mock('../../../../src/classes/Globals', () => {
-	return {
-		BROWSER_INFO: { displayName: 'Chrome' },
-		EXTENSION_VERSION: '8.3.2'
-	};
-});
-
-jest.mock('../../utils/msg', () => props => ({
-	sendMessage: jest.fn(),
+jest.mock('../../../../src/classes/Globals', () => ({
+	BROWSER_INFO: { displayName: 'Chrome' },
+	EXTENSION_VERSION: '8.3.2'
 }));
 
 describe('app/panel/components/About', () => {
 	describe('Snapshot tests with react-test-renderer', () => {
-		test('about component is rendered correctly', () => {
+		test('About component is rendered correctly', () => {
 			const component = renderer.create(
 				<About />
 			).toJSON();
@@ -26,5 +19,4 @@ describe('app/panel/components/About', () => {
 		});
 	});
 });
-
 
